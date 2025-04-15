b08d08f2e67f0e446dff180c4a7ffbc7941a8edd
asherplotnik <aserplotnik@gmail.com>
2025-04-15 10:00:19 +0300

    fix image building mixup

--- services/service-provider/src/main/java/com/stuby/service/ServiceApplication.java
+++ services/service-provider/src/main/java/com/stuby/service/ServiceApplication.java
@@ -1,13 +1,13 @@
 package com.stuby.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class ServiceApplication {
-
 	public static void main(String[] args) {
+		log.info("<<<<< PROVIDER INSTANCE >>>>>");
 		SpringApplication.run(ServiceApplication.class, args);
 	}
-
 }
--- services/stubpod/src/main/java/com/stuby/stubpod/StubpodApplication.java
+++ services/stubpod/src/main/java/com/stuby/stubpod/StubpodApplication.java
@@ -1,12 +1,13 @@
 package com.stuby.stubpod;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class StubpodApplication {
-
 	public static void main(String[] args) {
+		log.info("<<<<< TEST SYSTEM INSTANCE >>>>>");
 		SpringApplication.run(StubpodApplication.class, args);
 	}
 
